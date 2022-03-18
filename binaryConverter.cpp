a0da2e6ff78060c72ebbb16b430159d8537b35e0
James A. Ware <jamesaware@rocketmail.com>
2022-03-18 17:53:20 +0000

    Add files via upload

--- binaryConverter.cpp
+++ binaryConverter.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
+
     int Octet_1 {0};
     int Octet_2 {0};
     int Octet_3 {0};
     int Octet_4 {0};
+    
+    int list[] = {128, 64, 32, 16, 8, 4, 2, 1};
+    int list2[] = {128, 64, 32, 16, 8, 4, 2, 1};
+    int list3[] = {128, 64, 32, 16, 8, 4, 2, 1};
+    int list4[] = {128, 64, 32, 16, 8, 4, 2, 1};
     
 void askForIP() {
     //asks for the IP address 
@@ -22,11 +27,11 @@ void askForIP() {
     cout <<"The IPv4 address you wish to conevrt is "<<Octet_1<<"."<<Octet_2<<"."<<Octet_3<<"."<<Octet_4<<" ias this correct?"<<endl;
 }
 
-
-
 int main () {
     //asks for IPv4 Input
     askForIP(); 
+    
+    //confirm your IP entry is correct
     char ynanswer = 'y';
     cout <<"y/n"<< endl;
     cin >> ynanswer ;
@@ -35,34 +40,56 @@ int main () {
     }else {
         cout << "OKIDOKI" << endl;
     }
-    string binOctet_1 = "";
     
-    //the int values that the converter will check off
-    int list[] = {128, 64, 32, 16, 8, 4, 2, 1};
+    int hnum2 {0};
+    
+    string binOctet_2 = "";
 
-    for (int i : list) {
-        if (i - Octet_1 >= 0) {
-            i = i - Octet_1; binOctet_1.append("1");
+    for (hnum2 = Octet_2; int i : list2){
+        if (hnum2 - i >= 0){
+            hnum2 = hnum2 - i;binOctet_2.append("1");
         } else {
-            binOctet_1.append("0");
+            binOctet_2.append("0");
         }
     }
-    cout << binOctet_1;
 
+    int hnum3 {0};
+    
+    string binOctet_3 = "";
+
+    for (hnum3 = Octet_3; int i : list3){
+        if (hnum3 - i >= 0){
+            hnum3 = hnum3 - i;binOctet_3.append("1");
+        } else {
+            binOctet_3.append("0");
+        }
+    }
 
     int hnum {0};
-    int list[] = {128, 64, 32, 16, 8, 4, 2, 1};
+    
     string binOctet_1 = "";
 
     for (hnum = Octet_1; int i : list){
         if (hnum - i >= 0){
-            hnum = hnum - i;binOctet_1.append("1"); cout << i << " i val " <<hnum<<" hnum"<<endl;
+            hnum = hnum - i;binOctet_1.append("1");
         } else {
             binOctet_1.append("0");
         }
     }
-    cout << binOctet_1;
+
+    int hnum4 {0};
     
-    
+    string binOctet_4 = "";
+
+    for (hnum4 = Octet_4; int i : list4){
+        if (hnum4 - i >= 0){
+            hnum4 = hnum4 - i;binOctet_4.append("1");
+        } else {
+            binOctet_4.append("0");
+        }
+    }
+
+    cout << "The Converted IPv4 is"<<endl;
+    cout << binOctet_1<<"."<<binOctet_2<<"."<<binOctet_3<<"."<<binOctet_4<<endl;
     return 0;
 }
